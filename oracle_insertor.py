8b60db60d5faac971464911a33bcd93f5b048a1d
shafiq-excelsior <shafiq.mohammed@excelsior.edu>
2024-09-17 00:00:13 +0100

    Add files via upload

---
+++ oracle_insertor.py
@@ -0,0 +1,45 @@
+from dotenv import load_dotenv
+import cx_Oracle
+import glob
+import os
+
+load_dotenv()
+
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_hostname = os.getenv('DB_HOSTNAME')
+db_port = os.getenv('DB_PORT')
+db_service_name = os.getenv('DB_SERVICE_NAME')
+
+dsn = cx_Oracle.makedsn(db_hostname, db_port, service_name=db_service_name)
+
+connection = cx_Oracle.connect(
+    user=db_user,
+    password=db_password,
+    dsn=dsn
+)
+
+cursor = connection.cursor()
+
+view_files = glob.glob('*.sql')
+
+for view_file in view_files:
+    with open(view_file, 'r') as file:
+        view_query = file.read()
+        
+        try:
+            cursor.execute(view_query)
+            
+            results = cursor.fetchall()
+            
+            print(f"Results for {view_file}:")
+            for row in results:
+                print(row)
+            
+            print(f"Successfully executed query from {view_file}")
+        except cx_Oracle.DatabaseError as e:
+            error, = e.args
+            print(f"Error executing query from {view_file}: {error.message}")
+
+cursor.close()
+connection.close()
