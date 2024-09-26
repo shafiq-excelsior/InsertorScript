a006d626004ba84ebe32112871dd8a713dcfdbac
shafiq-excelsior <shafiq.mohammed@excelsior.edu>
2024-09-26 18:26:45 +0100

    Add files via upload

--- oracle_insertor.py
+++ oracle_insertor.py
@@ -1,9 +1,26 @@
 from dotenv import load_dotenv
+from datetime import datetime
 import oracledb
+import logging
 import glob
 import os
 
 load_dotenv()
+
+log_dir = 'logs'
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+    
+log_filename = os.path.join(log_dir, datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '.log')
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler(log_filename),
+        logging.StreamHandler()
+    ]
+)
 
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
@@ -21,25 +38,32 @@ connection = oracledb.connect(
 
 cursor = connection.cursor()
 
-view_files = glob.glob('*.sql')
+view_folders = glob.glob(os.path.join('Views', 'View*'))
 
-for view_file in view_files:
-    with open(view_file, 'r') as file:
-        view_query = file.read()
+for view_folder in view_folders:
+    view_files = glob.glob(os.path.join(view_folder, '*.sql'))
+    working_bool = True
+    for view_file in view_files:
+        with open(view_file, 'r') as file:
+            view_query = file.read()
+            logging.info(f"{view_folder}:")
+            try:
+                cursor.execute(view_query[:-1])
+                
+                logging.info(f"[{view_file}] Query executed successfully")
+            except oracledb.DatabaseError as e:
+                working_bool = False
+                error, = e.args
+                logging.info(f"[{view_file}] Error executing query: {error.message}")
+
+    output_dir = os.path.split(view_folder)
+    output_filename = output_dir[-1].split('_')
+    if working_bool:
+        logging.info(f"{view_folder}: All queries executed successfully")
+    else:
+        output_dir[-1] = "InProgress_" + '_'.join(output_filename[:1])
         
-        try:
-            cursor.execute(view_query)
-            
-            results = cursor.fetchall()
-            
-            print(f"Results for {view_file}:")
-            for row in results:
-                print(row)
-            
-            print(f"Successfully executed query from {view_file}")
-        except oracledb.DatabaseError as e:
-            error, = e.args
-            print(f"Error executing query from {view_file}: {error.message}")
-
+    os.makedirs(os.path.join(*output_dir))
+    
 cursor.close()
 connection.close()
